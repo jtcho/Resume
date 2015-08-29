3103344c9edeca4f7484dcb829c14a80a96b8ea6
JT Cho <jonathan.t.cho@gmail.com>
2015-08-28 22:42:06 -0400

    Added clean all script.

---
+++ cleanall.sh
@@ -0,0 +1,6 @@
+
+find . -name "*.aux" -type f -delete
+find . -name "*.log" -type f -delete
+find . -name "*.out" -type f -delete
+find . -name "*.pyg" -type f -delete
+find . -name "*.listing" -type f -delete
